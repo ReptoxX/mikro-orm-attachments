e044da4b5534f0ea98eab85417ef7e4418f6ef18
Julien Terzibaschian <jt@e-confirm.de>
2026-01-24 17:49:48 +0100

    feat: included workflow to publish to npm

---
+++ packages/mikro-orm-attachments/build.ts
@@ -0,0 +1,15 @@
+import { $ } from "bun";
+import { existsSync } from "fs";
+import { rm } from "fs/promises";
+
+const outDir = "./dist";
+
+if (existsSync(outDir)) {
+	await rm(outDir, { recursive: true });
+}
+
+await $`bun build ./src/index.ts --outdir ${outDir} --format esm --target node --minify --sourcemap --external @mikro-orm/core --external sharp --external blurhash --external flydrive --external file-type --external uuid --outfile ${outDir}/index.js`;
+
+await $`bunx tsc --emitDeclarationOnly --declaration --declarationMap --outDir ${outDir}`;
+
+console.log("Build completed successfully!");
